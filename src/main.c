7610c15c6075de4bde1a9569c083e0df3ec4f6a4
tracer <tracer@njuoslab.org>
2014-06-28 22:18:58 +0800

    > play

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 #include "device.h"
 void grading (void);
 void test(void);
+void test_producer(void);
 
 void
 entry(void) {
@@ -14,18 +15,67 @@ entry(void) {
 	init_thread();
 	init_tty();
 	enable_interrupt();
-	test();
-//	create_kthread(grading);
+	create_kthread(test_producer);
 	while (1) {
 		wait_for_interrupt();
 	}
 	assert(0);
 }
-
+#define NBUF 5
+#define NR_PROD 3
+#define NR_CONS 4
+ 
+int buf[NBUF], f = 0, r = 0, g = 1;
+int last = 0;
+Semaphore empty, full, mutex;
+ 
+void
+test_producer(void) {
+	while (1) {
+		P(&mutex);
+		P(&empty);
+		if(g % 10000 == 0) {
+			printk(".");	// tell us threads are really working
+		}
+		buf[f ++] = g ++;
+		f %= NBUF;
+		V(&full);
+		V(&mutex);
+	}
+}
+ 
+void
+test_consumer(void) {
+	int get;
+	while (1) {
+		P(&mutex);
+		P(&full);
+		get = buf[r ++];
+		assert(last == get - 1);	// the products should be strictly increasing
+		last = get;
+		r %= NBUF;
+		V(&empty);
+		V(&mutex);
+	}
+}
+ 
+void
+test_setup(void) {
+	init_sem(&full, 0);
+	init_sem(&empty, NBUF);
+	init_sem(&mutex, 1);
+	int i;
+	for(i = 0; i < NR_PROD; i ++) {
+		wakeup(create_kthread(test_producer));
+	}
+	for(i = 0; i < NR_CONS; i ++) {
+		wakeup(create_kthread(test_consumer));
+	}
+}
 
 void
 grading (void) {
-
+	
 }
 
 void
