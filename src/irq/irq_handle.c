17baed564bed2cac50cf89b98dd365c3f609dcd5
tracer <tracer@njuoslab.org>
2014-07-04 12:53:26 +0800

    > play

--- src/irq/irq_handle.c
+++ src/irq/irq_handle.c
@@ -9,12 +9,13 @@ irq_handle(struct TrapFrame *tf) {
 	is_irq = 1;
 	current->tf = tf;
 	if (tf->irq == 1000) {
+		schedule();
+		next = current;
 		update_sched();
 		update_jiffy();
 		send_updatemsg();
-		schedule();
-		next = current;
 	} else if (tf->irq == 1001) {
+		send_keymsg();
 		uint32_t code = in_byte(0x60);
 		uint32_t val = in_byte(0x61);
 		out_byte(0x61, val | 0x80);
@@ -23,7 +24,6 @@ irq_handle(struct TrapFrame *tf) {
 		putchar('0' + code / 10 % 10);
 		putchar('0' + code % 10);
 		putchar('\n');
-		send_keymsg();
 	} else if (tf->irq == 0x80) {
 		current = next;
 	} else {
